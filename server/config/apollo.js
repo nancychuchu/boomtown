a4411d286c971c8e153c07e5dbce5c380604f81d
Nancy <Kitty@Charlies-MacBook-Pro.local>
2019-04-18 15:27:44 -0700

    wire schema to resolver index.js with makeExecutableSchema

--- server/config/apollo.js
+++ server/config/apollo.js
@@ -1,25 +1,16 @@
+//jshint esversion: 6
+
 const { ApolloServer } = require('apollo-server-express');
 const { makeExecutableSchema } = require('graphql-tools');
 
-const typeDefs = require('../api/schema');
+
+const typeDefs = require('../api/schema'); //type definitiions fancy desig for schema
 let resolvers = require('../api/resolvers');
 
 module.exports = ({ app, pgResource }) => {
   resolvers = resolvers(app);
 
-  /**
-   * @TODO: Initialize Apollo Server
-   *
-   * Once you've defined your schema types, it's time to wire up your schema
-   * to your resolving functions. This is Apollo magic, and it's done using
-   * the 'makeExecutableSchema' function provided by the 'graphql-tools' package.
-   *
-   * https://www.apollographql.com/docs/apollo-server/v2/api/graphql-tools.html#makeExecutableSchema
-   */
-
-  // @TODO: Refactor to use 'makeExecutableSchema' to wire up your schema to your resolvers:
-  const schema = undefined;
-  // -------------------------------
+  const schema = makeExecutableSchema({typeDefs, resolvers});
 
   const apolloServer = new ApolloServer({
     context: ({ req }) => {
@@ -42,6 +33,7 @@ module.exports = ({ app, pgResource }) => {
          * Refactor this code and supply any additional information (values, methods, objects...etc)
          * you'll need to use in your resolving functions.
          */
+        pgResource
       };
     },
     schema
