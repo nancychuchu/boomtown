c912befd5561716fe6c70c4159dee1abc0500a4b
Nancy <Kitty@Charlies-MacBook-Pro.local>
2019-04-18 15:25:47 -0700

    connect to postgres sql database with configuration variables

--- server/config/postgres.js
+++ server/config/postgres.js
@@ -1,15 +1,19 @@
+//jshint esversion: 6
+
 const { Pool } = require('pg');
 
 module.exports = (app) => {
-  /**
-   * @TODO: Configuration Variables
-   *
-   *  Retrieve the necessary information to connect to Postgres
-   *  For example: app.get('PG_DB')
-   */
-  return new Pool({
-    /**
-     *  @TODO: Supply the correct configuration values to connect to postgres
-     */
+
+   const c =  new Pool({
+    host: app.get('PG_HOST'),
+    user: app.get('PG_USER'),
+    password: app.get('PG_PASSWORD'),
+    database: app.get('PG_DB'),
+    //define two properties to tell our libraries that if there's a bit of a delay, it's okay
+    idleTimeoutMillis: 30000, //if something takes more than 3s to connect, break. 
+    connectionTimeoutMillis: 2000, // when connection is made, and nothing happens after 2s, break connection.
   });
+  
+  return c; 
+  
 };
