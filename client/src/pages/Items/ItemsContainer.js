93f3e047b5f8f9f6818ca275d8633bd6dca82192
Nancy <Kitty@Charlies-MBP.hitronhub.home>
2019-04-25 00:15:35 -0700

    add query to pass items as props to Items component

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -3,15 +3,24 @@ import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 // import FullScreenLoader from '../../components/FullScreenLoader';
-// import { Query } from 'react-apollo';
-// import { } from '../../apollo/queries';
+import { ALL_ITEMS_QUERY } from '../../apollo/queries';
+import { Query } from 'react-apollo';
 
 class ItemsContainer extends Component {
-    render() {
-        return (
-            <Items />
-        );
-    }
+  render() {
+    return (
+      <Query query={ALL_ITEMS_QUERY} variables={{ filter: 2 }}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...'; //later we'll change Loading... to a loading screen.
+          if (error) return `Error! ${error.message}`;
+          if (data) {
+            console.log(data);
+            return <Items classes={this.props.classes} items={data.items} />;
+          }
+        }}
+      </Query>
+    );
+  }
 }
 
 export default withStyles(styles)(ItemsContainer);
