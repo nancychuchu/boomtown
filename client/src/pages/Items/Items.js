f954f5fdb6e2ddee621cb3e4ee3258132635ec3b
Nancy <Kitty@Charlies-MBP.hitronhub.home>
2019-04-25 00:54:30 -0700

    test items display and import/return ItemsGrid component

--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,14 +1,28 @@
+import ItemsGrid from '../../components/ItemsGrid';
 import React from 'react';
+//import Gravatar from 'react-gravatar';
 
-const Items = ({ classes }) => {
+const Items = ({ classes, items }) => {
   return (
-    <div>
-      <p>
-        This is the items page located at <code>/items</code>.
-      </p>
-    </div>
+    // <div>
+    //   <p>
+    //     This is the items page located at <code>/items</code>.
+    //   </p>
+    //   {items.map(item => (
+    //     <div key={item.id}>
+    //       <h2>{item.title}</h2>
+    //       <p>
+    //         id: {item.id} <br />
+    //         description: {item.description}
+    //       </p>
+    //     </div>
+    //   ))}
+    //   <h1>THIS IS MY GRAVATAR!!!</h1>
+    //   <Gravatar email="nancy.q.chu@gmail.com" />
+    // </div>
+
+    <ItemsGrid items={items} />
   );
 };
 
 export default Items;
-
