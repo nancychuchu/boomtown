bf951d49b4854cf849f35dae9ecbbfd1b0b0fe55
Nancy <Kitty@Charlies-MBP.hitronhub.home>
2019-04-28 23:17:26 -0700

    clean extra comments and trial code

--- client/src/pages/Home/HomeContainer.js
+++ client/src/pages/Home/HomeContainer.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import Home from './Home';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import { Query } from 'react-apollo';
-// import { ALL_ITEMS_QUERY } from '../../apollo/queries';
-//import gql from 'graphql-tag';
+
 
 class HomeContainer extends Component {
   render() {
--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,28 +1,8 @@
 import ItemsGrid from '../../components/ItemsGrid';
 import React from 'react';
-//import Gravatar from 'react-gravatar';
 
 const Items = ({ classes, items }) => {
-  return (
-    // <div>
-    //   <p>
-    //     This is the items page located at <code>/items</code>.
-    //   </p>
-    //   {items.map(item => (
-    //     <div key={item.id}>
-    //       <h2>{item.title}</h2>
-    //       <p>
-    //         id: {item.id} <br />
-    //         description: {item.description}
-    //       </p>
-    //     </div>
-    //   ))}
-    //   <h1>THIS IS MY GRAVATAR!!!</h1>
-    //   <Gravatar email="nancy.q.chu@gmail.com" />
-    // </div>
-
-    <ItemsGrid items={items} />
-  );
+  return <ItemsGrid items={items} />;
 };
 
 export default Items;
