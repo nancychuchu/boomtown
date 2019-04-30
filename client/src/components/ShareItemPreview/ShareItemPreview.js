c83b744dc06c7f4dabfe7134f47d157042404c11
Nancy <Kitty@Charlies-MacBook-Pro.local>
2019-04-30 12:53:02 -0700

    replace copied item card code with ItemCard component

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -1,62 +1,16 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core';
-import styles from './styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Gravatar from 'react-gravatar';
-// import Moment from 'moment';
+import ItemCard from '../ItemCard';
+// import PropTypes from 'prop-types';
 
-const ItemPreview = ({ classes }) => {
-  // const dateFrom = Moment(
-  //   item.created.substring(0, 9).replace('-', '/')
-  // ).fromNow();
+import { connect } from 'react-redux';
 
-  return (
-    <Card className={classes.card}>
-      {/* <CardActionArea> */}
-      <CardMedia
-        className={classes.media}
-        image="https://loremflickr.com/320/240/dog"
-        title="A look at the item "
-      />
-
-      <CardContent className={classes.mainContainer}>
-        <CardContent className={classes.ownerSection}>
-          <Gravatar className={classes.gravatar} email="trial@gmail.com" />
-
-          <CardContent className={classes.ownerInfo}>
-            <Typography>To be filled </Typography>
-            <Typography variant="caption">Many years ago</Typography>
-          </CardContent>
-        </CardContent>
-
-        <Typography gutterBottom variant="headline" component="h2">
-          New Item
-        </Typography>
-
-        <Typography variant="caption" className={classes.tags}>
-          To be determined
-        </Typography>
-
-        <Typography component="p">This will autofill. </Typography>
-      </CardContent>
-      {/* </CardActionArea> */}
-      <CardActions>
-        <Button
-          className={classes.button}
-          variant="outlined"
-          size="small"
-          color="primary"
-        >
-          Borrow
-        </Button>
-      </CardActions>
-    </Card>
-  );
+const ShareItemPreview = ({ shareItemPreview }) => {
+  console.log(shareItemPreview);
+  return <ItemCard item={shareItemPreview} />;
 };
 
-export default withStyles(styles)(ItemPreview);
+//map reducer from redux as a prop to react.
+//Don't need to import it because connect from react-redux already connects the store to the react component
+const mapStateToProps = ({ shareItemPreview }) => ({ shareItemPreview });
+
+export default connect(mapStateToProps)(ShareItemPreview);
--- client/src/components/ShareItemPreview/styles.js
+++ client/src/components/ShareItemPreview/styles.js
@@ -1,39 +1,6 @@
 const styles = {
   card: {
-    width: 450,
-    height: 600,
-    margin: 150,
-    position: 'relative'
-  },
-  media: {
-    height: 300
-  },
-  gravatar: {
-    borderRadius: '50%'
-  },
-  mainContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%'
-  },
-  ownerSection: {
-    display: 'flex',
-    padding: 0,
-    marginBottom: 10
-  },
-  ownerInfo: {
-    paddingLeft: 10,
-    paddingTop: 10
-  },
-  tags: {
-    display: 'flex',
-    padding: 0
-    // whiteSpace: 'nowrap'
-  },
-  button: {
-    position: 'absolute',
-    color: 'black',
-    bottom: 20
+    width: 800
   }
 };
 
