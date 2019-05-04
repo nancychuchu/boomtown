edf3da26e20a8e19440f7f1521a9da7d4de2d65e
Nancy <Kitty@Charlies-MacBook-Pro.local>
2019-05-03 18:42:00 -0700

    wrap items query around viewer query for id to omit.

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
-// import FullScreenLoader from '../../components/FullScreenLoader';
+import FullScreenLoader from '../../components/FullScreenLoader';
 import { ALL_ITEMS_QUERY } from '../../apollo/queries';
+import { VIEWER_QUERY } from '../../apollo/queries';
 import { Query } from 'react-apollo';
+
 
 class ItemsContainer extends Component {
   render() {
     return (
-      <Query query={ALL_ITEMS_QUERY} variables={{ filter: 2 }}>
+      <Query query={VIEWER_QUERY}>
         {({ loading, error, data }) => {
-          if (loading) return 'Loading...'; //later we'll change Loading... to a loading screen.
+          if (loading) return <FullScreenLoader />;
           if (error) return `Error! ${error.message}`;
           if (data) {
-            console.log(data);
-            return <Items classes={this.props.classes} items={data.items} />;
+            return (
+              <Query
+                query={ALL_ITEMS_QUERY}
+                variables={{ filter: data.viewer.id }}
+              >
+                {({ loading, error, data }) => {
+                  if (loading) return <FullScreenLoader />;
+                  if (error) return `Error! ${error.message}`;
+                  if (data) {
+                    console.log(data);
+                    return (
+                      <Items classes={this.props.classes} items={data.items} />
+                    );
+                  }
+                }}
+              </Query>
+            );
           }
         }}
       </Query>
