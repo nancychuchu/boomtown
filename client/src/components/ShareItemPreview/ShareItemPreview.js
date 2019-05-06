ef61eba97ae26daf604f7856dc0ed701c72ad2e4
Nancy <Kitty@Charlies-MBP.hitronhub.home>
2019-05-05 20:26:31 -0700

    fix tag label and button styling

--- client/src/components/ShareItemPreview/ShareItemPreview.js
+++ client/src/components/ShareItemPreview/ShareItemPreview.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ItemCard from '../ItemCard';
+import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 
@@ -7,6 +8,17 @@ const ShareItemPreview = ({ shareItemPreview }) => {
   return <ItemCard item={shareItemPreview} />;
 };
 
+ShareItemPreview.propType = {
+  shareItemPreview: PropTypes.shape({
+    borrower: PropTypes.string,
+    description: PropTypes.string.isRequired,
+    id: PropTypes.string,
+    imageurl: PropTypes.string,
+    itemowner: PropTypes.objectOf(PropTypes.string.isRequired),
+    tags: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.string.isRequired)),
+    title: PropTypes.string.isRequired
+  })
+};
 //map reducer from redux as a prop to react.
 //Don't need to import it because connect from react-redux already connects the store to the react component
 const mapStateToProps = ({ shareItemPreview }) => ({ shareItemPreview });
